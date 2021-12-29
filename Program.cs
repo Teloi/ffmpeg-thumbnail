0e458a7ffb498caa9fc14ef75742eb81ce0be376
teloi <99101154@qq.com>
2021-12-29 12:14:18 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using FFmpeg.NET;
+
+namespace FFmpeg_Thumbnail
+{
+    internal class Program
+    {
+        private static readonly JsonConfigHelper JsonReader = new JsonConfigHelper("config.json");
+        private static readonly int GROUND_WIDTH = Convert.ToInt32(JsonReader.GetValue("GroundWidth"));
+        private static readonly int GROUND_HEIGHT = Convert.ToInt32(JsonReader.GetValue("GroundHeight"));
+        private static readonly int ROW_COUNT = Convert.ToInt32(JsonReader.GetValue("RowCount"));
+        private static readonly int COLUMN_COUNT = Convert.ToInt32(JsonReader.GetValue("ColumnCount"));
+        private static readonly string FFMPEG_PATH = JsonReader.GetValue("FFmpegPath");
+        private static readonly string INPUT_VIDEO = JsonReader.GetValue("InputVideo");
+        private static readonly string OUTPUT_PATH = JsonReader.GetValue("OutputPath");
+        private static readonly string OUTPUT_NAME = "-thumb";
+        private static readonly string OUTPUT_TEMP_NAME = "-thumb-temp";
+        private static readonly string OUTPUT_EXT = ".jpg";
+
+        public static async Task Main(string[] args)
+        {
+            await TransformVideo();
+        }
+
+        public static async Task<bool> TransformVideo()
+        {
+            Engine ffmpeg = new Engine(FFMPEG_PATH);
+            MediaFile inputFile = new MediaFile(INPUT_VIDEO);
+            MetaData metadata = await ffmpeg.GetMetaDataAsync(inputFile);
+            Bitmap bitmap = new Bitmap(GROUND_WIDTH, GROUND_HEIGHT);
+            string inputFileName = Path.GetFileNameWithoutExtension(INPUT_VIDEO);
+
+            double intervalTime = Math.Floor(metadata.Duration.TotalMilliseconds / (ROW_COUNT * COLUMN_COUNT + 1));
+            for (int i = 1; i < (ROW_COUNT * COLUMN_COUNT + 1); i++)
+            {
+                ConversionOptions options = new ConversionOptions { Seek = TimeSpan.FromMilliseconds(intervalTime * i) };
+                FileInfo fileInfo = new FileInfo(Path.Combine(OUTPUT_PATH, inputFileName + OUTPUT_TEMP_NAME + OUTPUT_EXT));
+                MediaFile file = await ffmpeg.GetThumbnailAsync(inputFile, new MediaFile(fileInfo), options);
+                Image tempThumbImage = Image.FromFile(file.FileInfo.FullName);
+                Image smallThumbImage = ImageHelper.ResizeImage(tempThumbImage, (GROUND_WIDTH / ROW_COUNT), (GROUND_HEIGHT / COLUMN_COUNT), true);
+                using (Bitmap bmp = new Bitmap(smallThumbImage))
+                {
+                    double col = Math.Floor(((double)i - 1) / ROW_COUNT) * (GROUND_HEIGHT / COLUMN_COUNT);
+                    double row = (i - 1) % ROW_COUNT * (GROUND_WIDTH / ROW_COUNT);
+                    using (Graphics draw = Graphics.FromImage(bitmap))
+                    {
+                        draw.DrawImage(bmp, (int)row, (int)col, bmp.Width, bmp.Height);
+                        draw.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                        Font font = new Font("Arial", 20, FontStyle.Regular);
+                        draw.DrawString(TimeSpan.FromMilliseconds(intervalTime * i).ToString(), font, new SolidBrush(Color.White), (int)row, (int)col);
+                    }
+                }
+
+                smallThumbImage.Dispose();
+                tempThumbImage.Dispose();
+                fileInfo.Delete();
+                Console.WriteLine(i);
+            }
+
+            bitmap.Save(Path.Combine(OUTPUT_PATH, inputFileName + OUTPUT_NAME + OUTPUT_EXT), System.Drawing.Imaging.ImageFormat.Jpeg);
+            bitmap.Dispose();
+
+            return true;
+        }
+    }
+}
---
+++ Utils/ImageHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg_Thumbnail
+{
+    internal class ImageHelper
+    {
+        public static Image ResizeImage(Image img, int width, int height, bool keepRatio = true)
+        {
+            if (img == null)
+            {
+                return null;
+            }
+            Bitmap bmp = new Bitmap(width, height);
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                graphics.Clear(System.Drawing.Color.Black);
+                if (keepRatio)
+                {
+                    Rectangle ns = getNewSize(img, width, height);
+                    graphics.DrawImage(img, ns, new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+                }
+                else
+                {
+                    graphics.DrawImage(img, new Rectangle(0, 0, width, height), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+                }
+            }
+            return bmp;
+        }
+
+        private static Rectangle getNewSize(Image img, int width, int height)
+        {
+            double ratio = Math.Max(img.Width / width * 1.0, img.Height / height * 1.0);
+            Size ns = new Size((int)(img.Width / ratio), (int)(img.Height / ratio));
+            Point offset = new Point((width - ns.Width) / 2, (height - ns.Height) / 2);
+
+            return new Rectangle(offset, ns);
+        }
+    }
+}
